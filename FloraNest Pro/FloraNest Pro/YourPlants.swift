bdcff203d7f0b9da6c373151f298a38af2452a34
Mae17240 <joshuamae01@gmail.com>
2025-11-11 17:39:41 +0000

    Your plant page updates
    + page layout
    + background
    
    - removed unused background images

--- FloraNest Pro/FloraNest Pro/YourPlants.swift
+++ FloraNest Pro/FloraNest Pro/YourPlants.swift
@@ -40,19 +40,138 @@ struct YourPlants: View {
                                 .frame(height: 220)
                                 .clipped()
                                 .cornerRadius(50, corners: [.topLeft, .topRight])
-                                .opacity(0.9)
+                                .opacity(0.8)
                             
-                            Image("LearnAboutFloraNest")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: 11)
-                                .padding(.bottom, 25)
-                                .opacity(0)
+                            
                         }
                     }
                     
-                    // White box with sections
+                    // Green Main Box
+                    
                     VStack(spacing: 10) {
+                        Spacer().frame(height: 24)
+                        
+                        // Your plants headder
+                        VStack {
+                            HStack() {
+                                Text("Ruby's")
+                                    .foregroundColor(.white)
+                                    .font(.dmSansExtraBold(size: 25))
+                                
+                                Text("Plants.")
+                                    .foregroundColor(.white)
+                                    .font(.dmMonoLight(size: 20))
+                                    .padding(.top, 2)
+                                
+                                    
+                                
+                                
+                                Spacer()
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 30)
+                            
+                            
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical, 10)
+                        
+                        // Section 2
+                        VStack {
+                            HStack(alignment: .top, spacing: 10) {
+                                Text("+")
+                                    .font(.dmSansRegular(size: 18))
+                                    .foregroundColor(.white)
+    
+                                
+                                Text("Plant Reminders !")
+                                    .font(.dmSansExtraBold(size: 18))
+                                    .foregroundColor(.white)
+                                    .padding(.top, 3)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 17)
+                            
+                            Text("You have 5 plants that require attention.")
+                                .font(.dmMonoRegular(size: 15))
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                                .padding(.top, -6)
+                                .padding(.horizontal, 20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical, 10)
+                        
+                        // Section 3
+                        VStack {
+                            HStack(alignment: .top, spacing: 10) {
+                                Text("+")
+                                    .font(.dmSansRegular(size: 18))
+                                    .foregroundColor(.white)
+                                    .padding(.top, 8)
+                                
+                                Text("Plants collected")
+                                    .font(.dmSansExtraBold(size: 18))
+                                    .foregroundColor(.white)
+                                    .padding(.top, 8)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 20)
+                            
+                            Text("You have collected 5 plants")
+                                .font(.dmMonoRegular(size: 15))
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                                .padding(.top, -6)
+                                .padding(.horizontal, 20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical, 10)
+                        
+                        // Section 4
+                        VStack {
+                            HStack(alignment: .top, spacing: 10) {
+                               
+                                
+                                Image("Cloud")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 20)
+                                
+                                Text("Weather in your area")
+                                    .font(.dmSansExtraBold(size: 18))
+                                    .foregroundColor(.white)
+                                
+                                    
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 20)
+                            
+                            Text("Rain for the next 3 hours")
+                                .font(.dmMonoRegular(size: 15))
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                                .padding(.top, -6)
+                                .padding(.horizontal, 20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .fixedSize(horizontal: false, vertical: true)
+                            
+                            Text("18°c")
+                                .font(.dmMonoRegular(size: 15))
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                                .padding(.top, -6)
+                                .padding(.horizontal, 20)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .fixedSize(horizontal: false, vertical: true)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.vertical, 10)
+                        
                         Spacer()
                         
                         // Footer with 3 buttons
@@ -79,7 +198,7 @@ struct YourPlants: View {
                                     
                                     Text("Home")
                                         .font(.dmMonoRegular(size: 20))
-                                        .foregroundColor(.black)
+                                        .foregroundColor(.white)
                                     
                                 }
                             }
@@ -91,7 +210,7 @@ struct YourPlants: View {
                                 VStack {
                                     Text(">")
                                         .font(.dmMonoRegular(size: 24))
-                                        .foregroundColor(Color.black)
+                                        .foregroundColor(Color.white)
                                 }
                                 .foregroundColor(Color(hex: hexColor) ?? Color.white)
                                 .frame(maxWidth: .infinity)
@@ -110,6 +229,7 @@ struct YourPlants: View {
                 }
             }
         }
+        .navigationBarHidden(true)
     }
 }
 
